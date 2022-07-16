506f249935ba6b5933814087db3fbe3fce0cfe18
Oklein1 <ofirklein2@gmail.com>
2022-07-16 18:28:54 -0400

    File Dump

---
+++ test/evolutionary_prog/core_test.clj
@@ -0,0 +1,7 @@
+(ns evolutionary-prog.core-test
+  (:require [clojure.test :refer :all]
+            [evolutionary-prog.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
